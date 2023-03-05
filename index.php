abb7e4af208c07b5acc37c82625cb689b6a2d97f
saiprasadcs <115007506+saiprasadcs@users.noreply.github.com>
2023-03-05 11:56:19 +0530

    initial design changes

--- index.php
+++ index.php
@@ -5,11 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Yinka Enoch Adedokun">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
+    <!--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>-->
+<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>-->
     <link rel="stylesheet" type="text/css" href="styles/common.css">
     <title>Login Page</title>
     
 </head>
-<!-- Main Content -->a
-<body >
+<!-- Main Content -->
+<body style="background-image: url('./assets/ola.png');background-repeat: none;background-size:cover;">
+        <div class="container">
+             <div class="containercard mx-5 p-5 text-center">
+         <body style="background-image: url('./assets/bg-3.webp');background-repeat: no-repeat;background-size:cover;">
+       <div class="container">
+        <div class="containercard mx-5 p-5 text-center">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">ADMIN</h5>
+                                <a href="pages/admin">
+                                   <button type="submit" value="Submit" class="btn" >LOGIN</button>
+                                </a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">CUSTOMER</h5>
+                                <a href="pages/customer">
+                                   <button type="submit" value="Submit" class="btn">LOGIN</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">DRIVER</h5>
+                               <a href="pages/driver">
+                                 <button type="submit" class="btn btn-dark ">LOGIN</button>
+                               </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+       </div>
+
 </body>
 </html>
---
+++ indexFinal.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="indexFinalStyle.css">
+    <title>DropDown</title>
+</head>
+        <div class="selector">
+                        <div id="selectedField">
+                            <p id="selectText">Job Role</p>
+                            <img src="./assets/arrow.png" alt="" id="arrowIcon">
+                        </div>
+                        <ul id="list" class="hide">
+                            <li class="options">
+                                <a href=""><p>Admin</p></a>
+                            </li>
+                            <li class="options">
+                                <a href=""><p>Driver</p></a>
+                            </li>
+                            <li class="options">
+                                <a href=""><p>Customer</p></a>
+                            </li>
+                        </ul>
+                        </div>  
+
+                        <script>
+        
+    </script>
+                        </body>
+</html>
