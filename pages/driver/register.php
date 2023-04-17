7f660ae609a5325a5b4a039bfdeeb543bac5a42b
saiprasadcs <115007506+saiprasadcs@users.noreply.github.com>
2023-04-17 12:44:39 +0530

    added upload function code in register driver page

--- pages/driver/register.php
+++ pages/driver/register.php
@@ -2,6 +2,59 @@
 session_start();
 include("../../connection.php");
 if (isset($_POST['first_name'])) {
+    $statusMsg = '';
+// File upload path
+    $targetDir = "../../uploads/";
+    $fileName = basename($_FILES["file"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+    $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+    if(!empty($_FILES["file"]["name"])){
+        // Allow certain file formats
+        $allowTypes = array('jpg','png','jpeg','gif','pdf');
+        if(in_array($fileType, $allowTypes)){
+            // Upload file to server
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+                // Insert image file name into database
+//            $insert = $connection->query("INSERT into images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
+                if(true){
+                    $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+                }else{
+                    $statusMsg = "File upload failed, please try again.";
+                    echo "<div class='form'>
+                  <h3>$statusMsg</h3><br/>
+                  <p class='link'>Click here to <a href='./index.php'>registration</a> again.</p>
+                  </div>";
+                    exit();
+                }
+            }else{
+                $statusMsg = "Sorry, there was an error uploading your file.";
+                echo "<div class='form'>
+                  <h3>$statusMsg</h3><br/>
+                  <p class='link'>Click here to <a href='./index.php'>registration</a> again.</p>
+                  </div>";
+                exit();
+            }
+        }else{
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+
+            echo "<div class='form'>
+                  <h3>$statusMsg</h3><br/>
+                  <p class='link'>Click here to <a href='./index.php'>registration</a> again.</p>
+                  </div>";
+            exit();
+        }
+    }else{
+        $statusMsg = 'Please select a file to upload.';
+        echo "<div class='form'>
+                  <h3>$statusMsg</h3><br/>
+                  <p class='link'>Click here to <a href='./index.php'>registration</a> again.</p>
+                  </div>";
+        exit();
+    }
+// Display status message
+
+
     $role_id = 3;
     $firstName = $_POST['first_name'];
     $lastName = $_POST['last_name'];
@@ -28,9 +81,9 @@ if (isset($_POST['first_name'])) {
         }else{
             if ($password == $cpassword) {
                 $sql = "INSERT INTO driver (role_id,first_name,last_name,email,password,phoneno,
-                    vehicle_number,licence_number,modal,capacity,pickup_from,pickup_to,cost_per_person)
+                    vehicle_number,licence_number,modal,capacity,pickup_from,pickup_to,cost_per_person, fileName)
                     VALUES ('$role_id','$firstName','$lastName','$email','$password', '$phoneno',
-                     '$vehicle_number','$licence_number','$modal','$capacity','$pickup_from','$pickup_to','$cost_per_person')";
+                     '$vehicle_number','$licence_number','$modal','$capacity','$pickup_from','$pickup_to','$cost_per_person', '$fileName')";
                 $result = mysqli_query($connection, $sql);
                 if ($result) {
                     echo "<div class='form'>
